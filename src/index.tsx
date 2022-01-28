7d5676da93c1cbc9d677b4da8224ef6b2fffeb4a
wjdgus9808 <dbswjdgus9808@gmail.com>
2022-01-28 10:05:17 +0900

    push

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { theme } from "./theme";
+import { ThemeProvider } from "styled-components";
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
